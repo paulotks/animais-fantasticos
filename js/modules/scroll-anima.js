1bde4d2eb9ddcb8c30d632cf234b731d044c2815
paulotks <81379525+paulotks@users.noreply.github.com>
2023-04-23 16:06:10 -0300

    adicionando debounce ao scroll

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -1,9 +1,11 @@
+import debounce from "./debounce.js";
+
 export default class AnimacaoScroll {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
 
-    this.checkDistance = this.checkDistance.bind(this);
+    this.checkDistance = debounce(this.checkDistance.bind(this), 50);
   }
 
   // pega a distancia de cada itm em relação ao topo do site
